f31104f669dc970cdd74670a3e538cc5733362d5
Daniil Laureanti <daniil.laureanti@gmail.com>
2022-10-03 11:32:34 +0200

    Data model

---
+++ app/src/main/java/com/laureanti/androidmonetisation/model/Country.kt
@@ -0,0 +1,10 @@
+package com.laureanti.androidmonetisation.model
+
+class Country(
+    val countryName: String?,
+    val capital: String?,
+    val flag: String?,
+    val population: String?,
+    val area: String?,
+    val region: String
+)
